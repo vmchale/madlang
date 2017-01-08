4df3eb875a0e6733a46b95daa60f3c5ed1d48605
Vanessa McHale <tmchale@wisc.edu>
2017-01-08 00:01:21 -0600

    test suite is better

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,10 +6,12 @@ import Text.Madlibs.Ana.Parse hiding (main)
 import Text.Madlibs.Internal.Types
 import Text.Madlibs.Internal.Utils
 import Text.Madlibs.Cata.Run
+import Text.Madlibs.Cata.SemErr
 import Text.Megaparsec
 import Control.Monad.IO.Class
 import Control.Monad.State
 import Data.Function
+import Control.Exception
 import qualified Data.Text as T
 
 main :: IO ()
@@ -23,6 +25,12 @@ main = hspec $ do
             parseTok `shouldSucceedOn` madComplexFile
         it "returns a correct string from the template when evaluating a token" $ do
             (testIO . run) exampleTok `shouldSatisfy` (\a -> on (||) (a ==) "heads" "tails")
+        it "throws exception when two `:return`s are declared" $ do
+            (parseTok `shouldFailOn` semErrFile) `shouldThrow` semErr
+            --this is still behaving weirdly but I don't care
+
+semErr :: Selector SemanticError
+semErr = const True
 
 exampleTok :: RandTok
 exampleTok = List [(1.0,List [(0.5,Value "heads"),(0.5,Value "tails")])]
@@ -50,3 +58,13 @@ madComplexFile = ":define person\
 \:return\
 \    0.8 person fortune\
 \    0.2 goodfortune"
+
+semErrFile :: T.Text
+semErrFile = ":define something\
+\    0.5 \"heads\"\
+\    0.5 \"tails\"\
+\:return\
+\    1.0 something\
+\:return\
+\    0.5 something\
+\    0.5 \"it doesn't matter b/c this is gonna blow up in our faces anyways\""
