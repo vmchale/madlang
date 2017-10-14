ec1b68e7a85372b1e493eeb76dd01f16a88ee711
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-10-14 00:55:37 -0500

    give up lol

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,7 +12,7 @@ main = hspec $ do
     describe "parseTok" $ do
         parallel $ it "parses a .mad string with modifiers" $ do
             file <- madFileBasic
-            parseTok "" [] [] file `shouldParse` (List [(1.0,List [(0.5,Value "HEADS"),(0.5,Value "tails")])])
+            parseTok "" [] [] file `shouldParse` List [(1.0,List [(0.5,Value "HEADS"),(0.5,Value "tails")])]
         parallel $ it "fails when quotes aren't closed" $ do
             file <- madFileFailure
             parseTok "" [] [] `shouldFailOn` file
@@ -25,7 +25,7 @@ main = hspec $ do
         parallel $ it "parses a function with defined categories" $ do
             file <- readFile' "test/templates/cat.mad"
             parseTok "" [] [] `shouldSucceedOn` file
-        parallel $ it "returns a correct string from the template when evaluating a token" $ do
+        parallel $ it "returns a correct string from the template when evaluating a token" $
             run exampleTok >>= (`shouldSatisfy` (\a -> on (||) (a ==) "heads" "tails"))
         parallel $ it "throws exception when two `:return`s are declared" $ do
             file <- semErrFile
@@ -37,24 +37,24 @@ main = hspec $ do
             file <- madVar
             parseTok "" [] [] `shouldFailOn` file `shouldThrow` anyException
     describe "runFile" $ do
-        parallel $ it "parses nested modifiers and modifiers on variables correctly" $ \_ -> do
-            runFile ["aa"] "test/templates/modifiers.mad" >>= (`shouldSatisfy` (\a -> any (a==) ["AAAaaa","AAAaa","AAaaa","AAaa"]))
-        parallel $ it "parses file with inclusions and modifiers on functions" $ \_ -> do
-            runFile [] "test/templates/include.mad" >>= (`shouldSatisfy` (\a -> any (a==) ["heads","tails","on its side"]))
-        parallel $ it "parses file with recursive inclusions" $ \_ -> do
-            runFile [] "test/templates/include-recursive.mad" >>= (`shouldSatisfy` (\a -> any (a==) ["HEADS","tails","on its side"]))
-        parallel $ it "runs on a file out of order" $ \_ -> do
-            runFile [] "test/templates/ordered.mad" >>= (`shouldSatisfy` (\a -> any (a==) ["heads","tails","one","two","three","third","fourth"]))
-    describe "readFileQ" $ do
-        parallel $ it "executes embedded code" $ do
-            runTest >>= (`shouldSatisfy` (\a -> any (a==) ["HEADS","tails"]))
-    describe "madlang" $ do
-        parallel $ it "provides a quasi-quoter" $ do
-            runTestQQ >>= (`shouldSatisfy` (\a -> any (a==) ["hello","goodbye"]))
+        parallel $ it "parses nested modifiers and modifiers on variables correctly" $ \_ ->
+            runFile ["aa"] "test/templates/modifiers.mad" >>= (`shouldSatisfy` (\a -> elem a ["AAAaaa","AAAaa","AAaaa","AAaa"]))
+        parallel $ it "parses file with inclusions and modifiers on functions" $ \_ ->
+            runFile [] "test/templates/include.mad" >>= (`shouldSatisfy` (\a -> elem a ["heads","tails","on its side"]))
+        parallel $ it "parses file with recursive inclusions" $ \_ ->
+            runFile [] "test/templates/include-recursive.mad" >>= (`shouldSatisfy` (\a -> elem a ["HEADS","tails","on its side"]))
+        parallel $ it "runs on a file out of order" $ \_ ->
+            runFile [] "test/templates/ordered.mad" >>= (`shouldSatisfy` (\a -> elem a ["heads","tails","one","two","three","third","fourth"]))
+    describe "readFileQ" $
+        parallel $ it "executes embedded code" $
+        runTest >>= (`shouldSatisfy` (\a -> any (a==) ["HEADS","tails"]))
+    describe "madlang" $
+        parallel $ it "provides a quasi-quoter" $
+        runTestQQ >>= (`shouldSatisfy` (\a -> any (a==) ["hello","goodbye"]))
 
 -- | Read a file in as a `Text`
 readFile' :: FilePath -> IO T.Text
-readFile' = (fmap T.pack) . readFile
+readFile' = fmap T.pack . readFile
 
 exampleTok :: RandTok
 exampleTok = List [(1.0,List [(0.5,Value "heads"),(0.5,Value "tails")])]
